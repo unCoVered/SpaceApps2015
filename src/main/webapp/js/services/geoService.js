3e437becf384c2673dd9e4fbdad09ce448a63bac
RecuencoJones <david.recuencogadea@gmail.com>
2015-04-12 00:16:40 +0200

    Refactor

--- src/main/webapp/js/services/geoService.js
+++ src/main/webapp/js/services/geoService.js
@@ -1,13 +1,14 @@
 angular.module('starter')
 
-    //===========================//
-    /////// RIVER FLOW DATA ///////
-    //===========================//
-
     .factory('geoService', function($http, API){
         return({
-            getData: getData
+            getData: getData,
+            placeBaseMap: placeBaseMap
         });
+
+        //===========================//
+        /////// RIVER FLOW DATA ///////
+        //===========================//
 
         function getData( swlat, swlon, nelat, nelon, date1, date1hours, date2, date2hours, has2dates ) {
 
@@ -16,6 +17,7 @@ angular.module('starter')
                         date1 + ":" + date1hours +
                         (has2dates ? "/" + date2 + ":" + date2hours : "");
 
+            alert(API.URL + params);
             $http({
                 /*method: "POST",
                 url: API.URL,
@@ -27,7 +29,6 @@ angular.module('starter')
                 }*/
                 method: "GET",
                 url: API.URL + params
-
             }).success(function(data){
                 console.log("Got data.");
                 return data;
@@ -36,5 +37,70 @@ angular.module('starter')
                 console.log(data);
                 return null;
             });
+        };
+
+        //===========================//
+        ////////// BASE MAP ///////////
+        //===========================//
+
+        function placeBaseMap($scope){
+
+            //===========================//
+            ////// LEAFLET INIT VARS //////
+            //===========================//
+
+            var MIN_ZOOM = 3;
+            var INIT_ZOOM = 5;
+            var MAX_ZOOM = 8;
+
+            // SPAIN, OLÉ, TORTILLA
+            var MAP_LAT = 41.68337;
+            var MAP_LON = -0.8883134;
+
+            //===========================//
+            ////// LEAFLET BASE MAP ///////
+            //===========================//
+
+            var map = L.map('map',{zoomControl: false}).setView([MAP_LAT, MAP_LON], INIT_ZOOM);
+
+            map.attributionControl.setPrefix('');
+
+            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                minZoom: MIN_ZOOM,
+                maxZoom: MAX_ZOOM,
+                crs: L.CRS.EPSG4326
+            }).addTo(map);
+
+            //===========================//
+            ///////// WORLD LIMIT /////////
+            //===========================//
+
+            map.bounds = [],
+                map.setMaxBounds([
+                    [84.953828, -179.653976],
+                    [-84.938342, 179.103279]
+                ]);
+
+            var bounds = new L.latLngBounds([84.953828, -179.653976], [-84.938342, 179.103279]);
+            map.fitBounds(bounds);
+
+            //===========================//
+            //////// AREA SELECTOR ////////
+            //===========================//
+
+            $scope.swlat = "";
+            $scope.swlon = "";
+            $scope.nelat = "";
+            $scope.nelon = "";
+
+            var areaSelect = L.areaSelect({width:800, height:500});
+            areaSelect.on("change", function() {
+                var bounds = this.getBounds();
+                $scope.swlat = bounds.getSouthWest().lat;
+                $scope.swlon = bounds.getSouthWest().lng;
+                $scope.nelat = bounds.getNorthEast().lat;
+                $scope.nelon = bounds.getNorthEast().lng;
+            });
+            areaSelect.addTo(map);
         };
     });
