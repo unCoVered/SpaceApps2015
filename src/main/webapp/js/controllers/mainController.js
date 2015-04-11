48e40a337d7f9999d4e54a295ae49e7031cb357d
RecuencoJones <david.recuencogadea@gmail.com>
2015-04-11 21:10:08 +0200

    Error validation completed

--- src/main/webapp/js/controllers/mainController.js
+++ src/main/webapp/js/controllers/mainController.js
@@ -45,22 +45,20 @@ angular.module('starter')
         ///////* AREA SELECTOR *///////
         //===========================//
 
-        var areaSelect = L.areaSelect({width:100, height:100});
+        $scope.swlat = "";
+        $scope.swlon = "";
+        $scope.nelat = "";
+        $scope.nelon = "";
+
+        var areaSelect = L.areaSelect({width:800, height:500});
         areaSelect.on("change", function() {
             var bounds = this.getBounds();
-            console.log(bounds.getSouthWest().lat + ", " + bounds.getSouthWest().lng);
-            console.log(bounds.getNorthEast().lat + ", " + bounds.getNorthEast().lng);
+            $scope.swlat = bounds.getSouthWest().lat;
+            $scope.swlon = bounds.getSouthWest().lng;
+            $scope.nelat = bounds.getNorthEast().lat;
+            $scope.nelon = bounds.getNorthEast().lng;
         });
         areaSelect.addTo(map);
-
-        //===========================//
-        //////* SUBMIT REQUEST *///////
-        //===========================//
-
-        $scope.submit = function(){
-            alert('they submitting, me alerting');
-        }
-
 
         //===========================//
         ///////////* HOURS *///////////
@@ -90,5 +88,76 @@ angular.module('starter')
                         "21:00", "21:15", "21:30", "21:45",
                         "22:00", "22:15", "22:30", "22:45",
                         "23:00", "23:15", "23:30", "23:45"
-                        ]
+                        ];
+
+        //===========================//
+        //////* SUBMIT REQUEST *///////
+        //===========================//
+
+        $scope.date1 = "";
+        $scope.date2 = "";
+        $scope.date1hours = $scope.hours[0];
+        $scope.date2hours = $scope.hours[0];
+        $scope.date1errors = false;
+        $scope.date2errors = false;
+        $scope.errorMessage = "";
+
+        $scope.submit = function(){
+
+            var url = "http://localhost:8080/rios/";
+            var has2dates = false;
+            var tmp1 = 0;
+            var tmp2 = 0;
+            $scope.hideErrorMessage();
+
+            if($scope.isValid($scope.date1)){
+                tmp1 = Date.parse($scope.date1);
+
+                if($scope.isValid($scope.date2)){
+                    tmp2 = Date.parse($scope.date2);
+                    if(tmp2 < tmp1){
+                        $scope.errorMessage = "Second date can't be earlier than first. ";
+                        $scope.date2errors = true;
+                        return;
+                    }else if(tmp2 == tmp1 && !$scope.validHours($scope.date1hours, $scope.date2hours)){
+                        $scope.errorMessage = "Second hour can't be earlier than first. ";
+                        $scope.date2errors = true;
+                        return;
+                    }
+                    has2dates = true;
+                }
+                url += $scope.swlat + "/" + $scope.swlon + "/" +
+                        $scope.nelat + "/" + $scope.nelon + "/" +
+                        tmp1 + ":" + $scope.date1hours +
+                        (has2dates ? "/" + tmp2 + ":" + $scope.date2hours : "");
+                console.log(url);
+            }else{
+                $scope.errorMessage = "First date is wrong. ";
+                $scope.date1errors = true;
+            };
+        };
+
+        $scope.isValid = function(data){
+            if(data == "" || data == null || data == undefined){
+                return false;
+            }else{
+                return true;
+            }
+        };
+
+        $scope.validHours = function(hour1, hour2){
+            var idx1 = $scope.hours.indexOf(hour1);
+            var idx2 = $scope.hours.indexOf(hour2);
+            if(idx1 <= idx2){
+                return true;
+            }else{
+                return false;
+            }
+        };
+
+        $scope.hideErrorMessage = function(){
+            $scope.date1errors = false;
+            $scope.date2errors = false;
+        };
+
     }]);
